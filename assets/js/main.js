48362d745e7b4fc7251f9e7585f7554604d0f474
MunkoBabudorzhiev <munko2387@gmail.com>
2020-09-29 18:18:32 +0800

    1.03

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
-
+	$('.nav__list').on('click', '.nav__item:not(.active)', function () {
+		$(this).addClass('active').siblings().removeClass('active').closest('.usefull__block').find('.block__content').removeClass('active').eq($(this).index()).addClass('active');
+	});
 	$('.dropdown').on('mouseover', function () {
 		$(this).children('.nav__link').addClass('active');
 		$(this).children('.dropdown__block').addClass('active')
